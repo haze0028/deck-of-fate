b05dc50769c5eac0191d8e37507b6da0044c4213
Greg Hazelton <haze9greg@gmail.com>
2021-04-25 14:31:58 -0400

    deck builder implemented

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,15 @@ $(document).ready(function () {
    let drawn = false
    let chosen = [];
    let number;
+   let list = cardDetails;
+   let filteredList = cardDetails;
+
 
    // fade in contents when page is loaded
    $('#wrapper').delay(1000).fadeIn(2000);
 
    // vanishes the selected card if one exists
-   const resetCard = () => {
+   const hideCard = () => {
       if (drawn) {
          $('.selected').fadeOut();
       }
@@ -18,28 +21,32 @@ $(document).ready(function () {
       var confirm = $('#confirm-input').val().trim().toLowerCase();
 
       $('#confirm-draw-modal').modal('hide'); // hide the modal
-      resetCard(); // fades away existing card
+      hideCard(); // fades away existing card
 
       if (confirm === 'yes') {
-         $('#loader').delay(500).fadeIn(300);
+         if (chosen.length === list.length) {
+            setTimeout(function () {
+               depleted();
+            }, 500)
+         } else {
+            $('#loader').delay(500).fadeIn(300);
+            setTimeout(function () {
+               $('#loader').fadeOut(300);
+               roll();
+            }, 5000) // length of time Loader is present
 
-         setTimeout(function () {
-            $('#loader').fadeOut(300);
-            roll();
-         }, 5000) // length of time Loader is present
-
-         drawn = true
+            drawn = true
+         }
       }
    }
 
+
    ////////////////////// ROLL
    const roll = () => {
-      number = Math.floor(Math.random() * 22);
+      number = Math.floor(Math.random() * list.length);
 
       if (!chosen.includes(number)) {
          createCard();
-      } else if (chosen.length === 22) {
-         depleted();
       } else {
          roll();
       }
@@ -49,11 +56,11 @@ $(document).ready(function () {
    const createCard = () => {
       const num = number;
       const str1 = "images/deck/"
-      const img = cardDetails[num].image.substring(cardDetails[num].image.lastIndexOf('/') + 1);
-      const title = cardDetails[num].title;
+      const img = list[num].image.substring(list[num].image.lastIndexOf('/') + 1);
+      const title = list[num].title;
       const source = str1.concat(title.toLocaleLowerCase() + '.png');
 
-      $('#card-img').attr('src', source).attr('alt', title);
+      $('#card-img').attr('src', source).attr('alt', title).css('display', 'block');
 
       setTimeout(function () {
          $('.selected').fadeIn(3000) // length of time for card to fade in 
@@ -71,12 +78,12 @@ $(document).ready(function () {
 
    ////////////////////// DECK DEPLETED
    const depleted = () => {
-      $('#card-title').text('Deck depleted');
       $('#card-img').hide();
+      $('#card-title').text('Deck depleted');
 
       setTimeout(function () {
          $('.selected').fadeIn(400)
-      }, 1000)
+      }, 500)
    }
 
    ////////////////////// SHUFFLE
@@ -95,6 +102,78 @@ $(document).ready(function () {
    }
 
 
+   ////////////////////// CARD SELECTOR
+   $('#basic13-btn').on('click', function () {
+      $('.card-checkbox input').each(function (index, element) {
+         $(this).prop('checked', false);
+      })
+      for (var i = 1; i <= 13; i++) {
+         (function (ind) {
+            setTimeout(function () {
+               $('#card' + ind + '-chk').prop('checked', true);
+            }, 30 * ind);
+         })(i);
+      }
+   })
+
+   $('#selectall-btn').on('click', function () {
+      for (var i = 1; i <= 22; i++) {
+         (function (ind) {
+            setTimeout(function () {
+               $('#card' + ind + '-chk').prop('checked', true);
+            }, 30 * ind);
+         })(i);
+      }
+   })
+
+   $('#deselectall-btn').on('click', function () {
+      for (var i = 22; i >= 1; i--) {
+         (function (ind) {
+            setTimeout(function () {
+               $('#card' + ind + '-chk').prop('checked', false);
+            }, 30 * ind);
+         })(i);
+      }
+   })
+
+   $('#build-confirm-btn').on('click', function () {
+      list = cardDetails;
+      let deleteList = []
+
+      $('.card-checkbox').each(function (index, element) {
+         if (!$(this).find('input').prop('checked')) {
+            deleteList.push(index);
+         }
+      })
+
+      filteredList = list.filter(function (value, index, arr) {
+         return !deleteList.includes(index);
+      })
+
+      list = filteredList;
+
+      $('#build-deck-modal').modal('hide');
+      hideCard();
+      $('#expended #exp-cards .frame').fadeOut();
+   })
+
+
+   ////////////////////// CONFIRM RESET
+   const confirmReset = () => {
+      chosen = [];
+      list = filteredList;
+      $('#reset-modal').modal('hide');
+      hideCard();
+      $('#expended #exp-cards .frame').remove();
+      $('#card-title').text('');
+      $('#snackbar').addClass('show');
+      // After 4 seconds, vanish the snackbar
+      setTimeout(function () {
+         $('#snackbar').removeClass('show');
+      }, 4000);
+   }
+
+
    ////////////////////// TRIGGERS
    $('#confirm-draw').submit(function (e) {
       e.preventDefault();
@@ -110,31 +189,25 @@ $(document).ready(function () {
 
    $('#draw-btn').on('click', function () {
       $('#confirm-draw-modal').modal('show')
-      $('#confirm-input').delay(1000).focus();
    })
 
    $('.deck-card').on('click', function () {
       $('#confirm-draw-modal').modal('show');
-      $('#confirm-input').delay(1000).focus();
+   });
+
+   $('#build-btn').on('click', function () {
+      $('#build-deck-modal').modal('show');
    });
 
    $('#reset-btn').on('click', function () {
       $('#reset-modal').modal('show');
    })
 
-   $('#modal-reset-btn').on('click', function () {
-      chosen = [];
-      $('#reset-modal').modal('hide');
-      resetCard();
-      $('#expended #exp-cards .frame').fadeOut();
-      $('#snackbar').addClass('show');
-      // After 4 seconds, remove the show class from snackbar
-      setTimeout(function () {
-         $('#snackbar').removeClass('show');
-      }, 4000);
-   })
+   $('#modal-reset-btn').on('click', confirmReset);
 
    $('#shuffle-btn').on('click', shuffleDeck);
+
+
 
 
 })
